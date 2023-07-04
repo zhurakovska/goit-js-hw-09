cdefc8eedf7c061701bab99517dfbbd514b76613
zhurakovska <n.sinawka@gmail.com>
2023-07-04 23:46:47 +0300

    done task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+function secondHomeWork() {
+  const formEl = document.querySelector('.form');
+
+  const onFormElSubmit = event => {
+    event.preventDefault();
+
+    console.dir(event.target.elements);
+
+    const { delay, step, amount } = event.target.elements;
+
+    console.log(amount.value);
+
+    for (let i = 1; i <= Number(amount.value); i++) {
+      createPromise(i, Number(delay.value), Number(step.value))
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    }
+
+    function createPromise(position, delay, step) {
+      const shouldResolve = Math.random() > 0.3;
+
+      const myPromise = new Promise((res, rej) => {
+        setTimeout(() => {
+          if (shouldResolve) {
+            res({ position, delay });
+          } else {
+            rej({ position, delay });
+          }
+        }, delay);
+      });
+      return myPromise;
+    }
+  };
+
+  formEl.addEventListener('submit', onFormElSubmit);
 }
+
+secondHomeWork();
